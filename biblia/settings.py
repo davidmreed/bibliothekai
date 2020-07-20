bde07a3f0705195d6e31a7fd32986572df52a198
David Reed <david@ktema.org>
2020-07-19 23:15:03 -0600

    Fix settings

--- biblia/settings.py
+++ biblia/settings.py
@@ -115,5 +115,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
