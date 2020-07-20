5c13df44c39a2f9f640196a459a830eb02620cf6
David Reed <david@ktema.org>
2020-07-19 23:06:38 -0600

    Fix settings

--- biblia/settings.py
+++ biblia/settings.py
@@ -75,6 +75,7 @@ WSGI_APPLICATION = 'biblia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
