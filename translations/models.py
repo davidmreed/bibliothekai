cc63d7fabaad9e941c2c19915c5b91e0ef149c5d
David Reed <david@ktema.org>
2020-10-05 18:09:21 -0600

    Add name for Series

--- translations/models.py
+++ translations/models.py
@@ -67,6 +67,9 @@ class Series(models.Model):
 
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Volume(models.Model):
     title = models.CharField(max_length=255)
