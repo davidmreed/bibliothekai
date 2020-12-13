e9306ddbc6139e5ac12dca719011416ad7f07c54
David Reed <david@ktema.org>
2020-12-12 18:09:43 -0700

    Add SLDS

--- lwc/src/index.js
+++ lwc/src/index.js
@@ -1,5 +1,8 @@
+// https://salesforce.stackexchange.com/questions/323613/lightning-open-source-use-of-base-components
+
 import { createElement } from 'lwc';
 import POC from 'bib/poc';
+import '@lwc/synthetic-shadow';
 
 const app = createElement('bib-poc', { is: POC });
 // eslint-disable-next-line @lwc/lwc/no-document-query
