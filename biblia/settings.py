eec6b3e12eaf41a9e9f1982cb632391e051c863a
David Reed <david@ktema.org>
2021-03-16 21:34:53 -0600

    Set test request format

--- biblia/settings.py
+++ biblia/settings.py
@@ -98,7 +98,8 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
-    ]
+    ],
+    "TEST_REQUEST_DEFAULT_FORMAT": "json",
 }
 
 MANIFEST_LOADER = {
