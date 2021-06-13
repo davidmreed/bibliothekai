8f61e4bbd5157d8decf3e728f49e4b756442de80
David Reed <david@ktema.org>
2021-06-13 11:14:56 -0600

    Fix class name

--- src/modules/bib/switch/switch.js
+++ src/modules/bib/switch/switch.js
@@ -1,6 +1,6 @@
 import { LightningElement, api } from 'lwc';
 
-export default class PopUpMenu extends LightningElement {
+export default class Switch extends LightningElement {
     _value;
 
     @api label;
