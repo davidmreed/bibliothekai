86003dbd1820991218eb619e98559dea562dcce4
David Reed <david@ktema.org>
2022-06-12 08:06:57 -0600

    Fix synthetic-shadow

--- src/addPublishedReview.js
+++ src/addPublishedReview.js
@@ -1,6 +1,6 @@
+import '@lwc/synthetic-shadow';
 import { createElement } from 'lwc';
 import AddPublishedReview from 'bib/addPublishedReview';
-import '@lwc/synthetic-shadow';
 
 const app = createElement('bib-add-published-review', {
     is: AddPublishedReview
